fdcc1d04b9303c863bba0e3a121bcbb11ca54439
slowthinker <80503808+hero-rq@users.noreply.github.com>
2022-03-16 20:25:59 +0900

    Create command_injection.py

---
+++ command_injection.py
@@ -0,0 +1,14 @@
+@APP.route('/ping', methods=['GET', 'POST'])
+def ping():
+    if request.method == 'POST':
+        host = request.form.get('host')
+        cmd = f'ping -c 3 "{host}"'
+        try:
+            output = subprocess.check_output(['/bin/sh', '-c', cmd], timeout=2)
+            return render_template('ping_result.html', data=output.decode('utf-8'))
+        except subprocess.TimeoutExpired:
+            return render_template('ping_result.html', data='Timeout !')
+        except subprocess.CalledProcessError:
+            return render_template('ping_result.html', data=f'an error occurred while executing the command. -> {cmd}')
+
+    return render_template('ping.html')
