725748860eed0034a0bbe9ee3055f45bf7c4e4c9
slowthinker <80503808+hero-rq@users.noreply.github.com>
2022-04-12 14:29:34 +0900

    Create basic_command.c

---
+++ basic_command.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <string.h>
+
+char name[16];
+
+char *command[10] = { "cat",
+    "ls -al",
+    "id paraschuutte",
+    "ps E",
+    "file ./rand" };
+void alarm_handler()
+{
+    puts("TIME OUT");
+    exit(-1);
+}
+
+void initialize()
+{
+    setvbuf(stdin, NULL, _IONBF, 0);
+    setvbuf(stdout, NULL, _IONBF, 0);
+
+    signal(SIGALRM, alarm_handler);
+    alarm(30);
+}
+
+int main()
+{
+    int idx;
+
+    initialize();
+
+    printf("Your name: ");
+    read(0, name, sizeof(name));
+    system(command[idx]);
+
+    return 0;
+}
