63e27a0cb342d8de7d80e9c34e912cdbc4b92dd2
slowthinker <80503808+hero-rq@users.noreply.github.com>
2022-04-18 08:34:50 +0900

    Update hand_writing 01.c

--- hand_writing 01.c
+++ hand_writing 01.c
@@ -1,27 +1,3 @@
-#include <stdio.h>
-
-unsigned long long factorial(unsigned int n) {
-  unsigned long long res = 1;
-  for (int i = 1; i <= n; i++) {
-    res *= i;
-  }
-  return res;
-}
-
-int main() {
-  unsigned int n;
-  unsigned int res;
-  printf("Input integer n: ");
-  scanf("%d", &n);
-  if (n >= 50) {
-    fprintf(stderr, "Input is too large");
-    return -1;
-  }
-  res = factorial(n);
-  printf("Factorial of N: %u\n", res);
-}
-
-//handalay
 factorial:
         push    rbp
         mov     rbp, rsp
