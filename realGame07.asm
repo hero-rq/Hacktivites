7156956e963288400f613251ad23445275b9c561
slowthinker <80503808+hero-rq@users.noreply.github.com>
2022-04-21 17:29:04 +0900

    Create realGame07.asm

---
+++ realGame07.asm
@@ -0,0 +1,23 @@
+.586
+.model flat, stdcall
+option casemap:none
+.code 
+main proc 
+push ebp 
+mov ebp, esp 
+mov eax, 10
+mov ebx, 11 
+add eax, ebx; 
+test eax, ebx 
+jz Exit
+Start:
+inc eax
+cmp eax, ebx
+je Exit
+ja Exit
+jmp Start
+Exit:
+pop ebp
+retn
+main endp
+end main
