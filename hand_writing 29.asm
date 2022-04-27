1977b3961c8c73d87cce4e10af5e47cc7c37d342
slowthinker <80503808+hero-rq@users.noreply.github.com>
2022-04-28 08:33:12 +0900

    Create hand_writing 29.asm

---
+++ hand_writing 29.asm
@@ -0,0 +1,35 @@
+swapNumber:
+        push    rbp
+        mov     rbp, rsp
+        mov     DWORD PTR [rbp-20], edi
+        mov     DWORD PTR [rbp-24], esi
+        mov     eax, DWORD PTR [rbp-20]
+        mov     DWORD PTR [rbp-4], eax
+        mov     eax, DWORD PTR [rbp-24]
+        mov     DWORD PTR [rbp-20], eax
+        mov     eax, DWORD PTR [rbp-4]
+        mov     DWORD PTR [rbp-24], eax
+        nop
+        pop     rbp
+        ret
+
+main:
+        push    rbp
+        mov     rbp, rsp
+        sub     rsp, 16
+        mov     DWORD PTR [rbp-4], 10
+        mov     DWORD PTR [rbp-8], 20
+        mov     edx, DWORD PTR [rbp-8]
+        mov     eax, DWORD PTR [rbp-4]
+        mov     esi, edx
+        mov     edi, eax
+        call    swapNumber
+        mov     edx, DWORD PTR [rbp-8]
+        mov     eax, DWORD PTR [rbp-4]
+        mov     esi, eax
+        mov     edi, OFFSET FLAT:.LC0
+        mov     eax, 0
+        call    printf
+        mov     eax, 0
+        leave
+        ret
