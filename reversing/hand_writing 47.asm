e116d94164bc6df7c9278ba1a089028bfba3c444
slow thinker <80503808+hero-rq@users.noreply.github.com>
2022-05-28 08:15:54 +0900

    Create hand_writing 47.asm

---
+++ reversing/hand_writing 47.asm
@@ -0,0 +1,29 @@
+        push    rbp
+        mov     rbp, rsp
+        sub     rsp, 16
+        mov     DWORD PTR [rbp-4], 2
+        mov     DWORD PTR [rbp-8], 7
+        mov     edx, DWORD PTR [rbp-4]
+        mov     eax, edx
+        add     eax, eax
+        add     eax, edx
+        mov     DWORD PTR [rbp-4], eax
+        mov     eax, DWORD PTR [rbp-8]
+        mov     edx, eax
+        shr     edx, 31
+        add     eax, edx
+        sar     eax
+        mov     DWORD PTR [rbp-8], eax
+        mov     eax, DWORD PTR [rbp-4]
+        mov     esi, eax
+        mov     edi, OFFSET FLAT:.LC0
+        mov     eax, 0
+        call    printf
+        mov     eax, DWORD PTR [rbp-8]
+        mov     esi, eax
+        mov     edi, OFFSET FLAT:.LC0
+        mov     eax, 0
+        call    printf
+        mov     eax, 0
+        leave
+        ret
