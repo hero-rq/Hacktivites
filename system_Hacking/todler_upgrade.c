f2b2d343c96c59dac6228068c68b0e32b17c8563
slow thinker <80503808+hero-rq@users.noreply.github.com>
2022-07-06 14:23:00 +0900

    Create todler_upgrade.c

---
+++ system_Hacking/todler_upgrade.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) {
+  int a, b; 
+
+  scanf("%d %d", &a, &b);
+
+  if (a == 0 || b == 0) {
+    printf("다시 숫자를 입력하세요");
+  }
+  
+  if (a > 0 && b > 0) {
+    printf("1");
+  } else if (a > 0 && b < 0) {
+    printf("4");
+  } else if (a < 0 && b < 0) {
+    printf("3");
+  } else if (a < 0 && b > 0) {
+    printf("2");
+  }
+  
+  return 0;
+}
