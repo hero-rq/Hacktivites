ae4839f5c0ced3e99e51079df51dfc2b196b8b1c
slow thinker <80503808+hero-rq@users.noreply.github.com>
2022-11-17 14:09:17 +0900

    Create slfjci.c

---
+++ system_Hacking/slfjci.c
@@ -0,0 +1,22 @@
+void main(void)
+
+{
+  undefined local_78 [104];
+  int local_10;
+  int local_c;
+  
+  setup();
+  banner();
+  local_c = 0xbadf00d;
+  local_10 = -0x11e2153;
+  printf("I need %x to %x\nAm I right? ",0xbadf00d,0xfee1dead);
+  __isoc99_scanf(&DAT_00100b66,local_78);
+  if ((local_c == 0xc0ff33) && (local_10 == 0xc0d3)) {
+    printf("Yes, I need %x to %x\n",0xc0ff33,0xc0d3);
+    system("/bin/sh");
+    return;
+  }
+  puts("I\'m feeling dead, coz you said I need bad food :(");
+                    /* WARNING: Subroutine does not return */
+  exit(0x539);
+}
